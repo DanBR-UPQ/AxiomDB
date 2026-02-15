95d2bbb53326e7175acbde55b7c7755825524d74
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-02-14 23:34:29 -0600

    Added pinning, dirty tracking to Page.java

--- storage/Page.java
+++ storage/Page.java
@@ -5,19 +5,42 @@ public class Page {
     
     private int pageId = 0;
     private byte[] data;
+    private boolean dirty;
+    private int pinCount;
 
     public Page(int pageId){
         this.pageId = pageId;
         this.data = new byte[PAGE_SIZE];
+        this.dirty = false;
+        this.pinCount = 0;
     }
 
 
+
+    public void pin(){
+        pinCount++;
+    }
+    public void unpin(){
+        pinCount--;
+    }
+
+
+
+    public void markDirty(){
+        dirty = true;
+    }
 
     // get methods
     public int getPageId(){
         return pageId;
     }
-    public byte[] getPageData(){
+    public byte[] getData(){
         return data;
+    }
+    public boolean isDirty(){
+        return dirty;
+    }
+    public int getPinCount(){
+        return pinCount;
     }
 }
