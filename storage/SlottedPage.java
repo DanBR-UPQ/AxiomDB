d0352acd6ba3445bd39b2bbf46927a81b110d6a4
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-02-16 23:27:26 -0600

    Added Record reading in SlottedPage

--- storage/SlottedPage.java
+++ storage/SlottedPage.java
@@ -51,32 +51,64 @@ public class SlottedPage {
     }
 
 
+    private int getPageSize() {
+        return page.getData().length;
+    }   
+
+
+    // ---------------- Reading / Accesing helpers ----------------
+    private int getSlotPosition(int slotIndex){ // slot 0 = 4096 - (0 + 1) * 4 = 4092, slot 0 is at 4092 bytes
+        return getPageSize() - (slotIndex + 1) * 4;
+    }
+
+    private int getSlotOffset(int slotIndex){ // slot 0 = record starts at 8 byters for example
+        int slotPosition = getSlotPosition(slotIndex);
+        return readInt(slotPosition);
+    }
+    // slotIndex is our record ID
+
+
+
+    // ---------------- CRUD ----------------
     public int insertRecord(byte[] recordData){
         int recordSize = recordData.length;
         int freeOffset = getFreeSpaceOffset();
         int recordCount = getRecordCount();
 
-        System.arraycopy(
-            recordData,
-            0,
-            page.getData(),
-            freeOffset,
-            recordSize
-        );
+
+        int recordWriteEnd = freeOffset + recordSize;
+        int slotPosition = getPageSize() - (recordCount + 1) * 4;
+
+        if (recordWriteEnd > slotPosition){
+            throw new RuntimeException("Page is full");
+        }
+
+
+        System.arraycopy(recordData, 0, page.getData(), freeOffset, recordSize);
 
         // slotPos = pageSize - (slotIndex + 1) * 4
         // eg slot 0 = 4096 - (0 + 1) * 4 = 4092
-        int slotPosition = getPageSize() - (recordCount + 1) * 4;
         writeInt(slotPosition, freeOffset);
+
 
         setRecordCount(recordCount + 1);
         setFreeSpaceOffset(recordSize + freeOffset);
         page.markDirty();
-        return freeOffset; // recordCount is our ID rn
+        return freeOffset; // freeOffset acts as our ID as it's where the data is stored rn
     }
 
-    private int getPageSize() {
-        return page.getData().length;
-    }   
 
+    public byte[] readRecord(int slotIndex, int recordSize){
+        byte[] record = new byte[recordSize];
+
+        if(slotIndex < 0 || slotIndex > getRecordCount()){
+            throw new IllegalArgumentException("Invalid index");
+        }
+
+        int recordOffset = getSlotOffset(slotIndex);
+
+        System.arraycopy(page.getData(), recordOffset, record, 0, recordSize);
+
+        return record;
+    }
 }
