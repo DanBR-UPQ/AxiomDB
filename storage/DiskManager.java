75ef1e20fc036e4071ca926c46d12971b224b101
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-02-21 20:10:37 -0600

    Added page loading to DiskManager

--- storage/DiskManager.java
+++ storage/DiskManager.java
@@ -8,7 +8,29 @@ public class DiskManager {
     public DiskManager(String fileName){
         try {
             this.file = new RandomAccessFile(fileName, "rw");
-        } catch (Exception e) {
+        } catch (java.io.IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    public Page loadPage(int pageId){
+        try {
+            long pageOffset = pageId * Page.PAGE_SIZE;
+
+            if (pageOffset >= file.length()){
+                throw new IllegalArgumentException("Page doesn't exist");
+            }
+
+            file.seek(pageOffset); // File points at pageOffset
+
+            Page page = new Page(pageId);
+            file.readFully(page.getData()); // copies stuff from file to our page's data, from pageOffset to data.len
+
+            return page;
+
+        } catch (java.io.IOException e) {
             throw new RuntimeException(e);
         }
     }
