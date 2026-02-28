e1679457ef3dbf019ebc739e77dafd4dc0bab18b
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-02-28 17:56:04 -0600

    Added RecordId for RID storage inside a dedicated class

---
+++ storage/RecordId.java
@@ -0,0 +1,12 @@
+public class RecordId {
+    private final int PageId;
+    private final int SlotId;
+
+    public RecordId(int PageId, int SlotId){
+        this.PageId = PageId;
+        this.SlotId = SlotId;
+    }
+
+    public int getPageId() { return PageId;}
+    public int getSlotId() { return SlotId;}
+}
