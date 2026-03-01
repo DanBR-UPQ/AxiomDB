e2a81d9448deeb08c7fa313e5410b5fc4f6eb2b6
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-02-28 22:34:11 -0600

    Added Table.java with insert logic

---
+++ storage/Table.java
@@ -0,0 +1,37 @@
+public class Table {
+    
+    private final DiskManager diskManager;
+
+    public Table(String fileName){
+        this.diskManager = new DiskManager(fileName);
+    }
+
+    public RecordId insert(byte[] data){
+
+        for (int i = 0; i < diskManager.getPageCount(); i++){
+
+            Page page = diskManager.loadPage(i);
+            SlottedPage slottedPage = new SlottedPage(page);
+
+            if (slottedPage.hasSpace(data.length)){ // pretending data.length is it's actual size for now
+            //Todo: Add proper space detection
+                
+                int SlotId = slottedPage.insertRecord(data);
+            
+                diskManager.writePage(page);
+
+                return new RecordId(page.getPageId(), SlotId);
+            }
+        }
+
+        Page newPage = diskManager.makeNewPage();
+        SlottedPage slottedPage = new SlottedPage(newPage);
+
+        int slotId = slottedPage.insertRecord(data);
+        diskManager.writePage(newPage);
+        return new RecordId(newPage.getPageId(), slotId);
+        
+
+    }
+
+}
