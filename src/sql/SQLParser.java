b1c8a175aa54f48291c29930a42afc03d89b1c9a
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-03-25 12:48:41 -0600

    Added parseInsert and parseSelect to SQLParser

--- src/sql/SQLParser.java
+++ src/sql/SQLParser.java
@@ -2,10 +2,9 @@ package sql;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import schema.Schema;
 import schema.Column;
 import schema.DataType;
+import schema.Schema;
 
 public class SQLParser {
     
@@ -34,6 +33,7 @@ public class SQLParser {
         }
     }
 
+    // --------------------- ACTUAL COMMANDS --------------------- 
     private SQLCommand parseCreateTable(String query){
         String rest = query.substring("CREATE TABLE".length()).trim();
 
@@ -83,4 +83,67 @@ public class SQLParser {
         Schema schema = new Schema(columns);
         return new CreateTableCommand(tableName, schema);
     }
+
+
+    private SQLCommand parseInsert(String query){
+        String rest = query.substring("INSERT INTO".length()).trim();
+
+        int valuesIndex = rest.toUpperCase().indexOf("VALUES");
+        if (valuesIndex == -1) {
+            throw new IllegalArgumentException("Invalid INSERT syntax");
+        }
+
+        String tableName = rest.substring(0, valuesIndex).trim();
+        String valuesPart = rest.substring(valuesIndex + "VALUES".length()).trim();
+
+        int openParen = valuesPart.indexOf("(");
+        int closeParen = valuesPart.lastIndexOf(")");
+
+        if (openParen == -1 || closeParen == -1) {
+            throw new IllegalArgumentException("Invalid VALUES syntax, missing parenthesis");
+        }
+
+        String inside = valuesPart.substring(openParen + 1, closeParen);
+        String[] rawValues = inside.split(",");
+
+        List<Object> values = new ArrayList<>();
+
+        for (String raw : rawValues) {
+            raw = raw.trim();
+
+            
+            if (raw.startsWith("\"") && raw.endsWith("\"")) {
+                String str = raw.substring(1, raw.length() - 1);
+                values.add(str);
+
+            } else {
+                try {
+                    int val = Integer.parseInt(raw);
+                    values.add(val);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Invalid value: " + raw);
+                }
+            }
+        }
+
+        return new InsertCommand(tableName, values);
+    }
+
+
+    private SQLCommand parseSelect(String query) {
+        String upper = query.toUpperCase();
+        
+        if (!upper.startsWith("SELECT * FROM")) {
+            throw new IllegalArgumentException("Only SELECT * FROM is supported");
+        }
+
+        String rest = query.substring("SELECT * FROM".length()).trim();
+
+        if (rest.isEmpty()) {
+            throw new IllegalArgumentException("Missing table name in SELECT");
+        }
+
+        return new SelectCommand(rest);
+    }
+    
 }
