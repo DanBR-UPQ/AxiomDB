109ae0f9156c095218b12f574327fa1facb1f5f0
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-03-24 23:07:12 -0600

    Created TableManager with get and create functions

---
+++ storage/TableManager.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TableManager {
+    private final Map<String, Table> tables;
+
+    public TableManager() {
+        this.tables = new HashMap<>();
+    }
+
+
+    public Table getTable(String tableName) {
+        if (tables.containsKey(tableName)) {
+            return tables.get(tableName);
+        }
+
+        TableMetadata metadata = TableMetadata.load(tableName);
+        Schema schema = metadata.getSchema();
+
+        // Table object is what has the .data file
+        String dataFileName = tableName + ".data";
+        Table table = new Table(dataFileName);
+
+        tables.put(tableName, table);
+        return table;
+    }
+
+
+    public void createTable(String tableName, Schema schema){
+
+        if (tables.containsKey(tableName)) {
+            throw new RuntimeException("Table already exists: " + tableName);
+        }
+
+        TableMetadata metadata = new TableMetadata(tableName, schema);
+        metadata.saveSchema();
+    }
+}
